509fd16a0ce4adacd41703bbcc6fd2802106e246
Nest0rr <elemele58@gmail.com>
2019-12-08 16:56:39 +0100

    Add some improvements to UI

--- WhenItsColdOutside/ViewController.swift
+++ WhenItsColdOutside/ViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var conditionImage: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
