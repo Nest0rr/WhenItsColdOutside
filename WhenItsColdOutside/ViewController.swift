4069438293e32e6eab26d8becfda78c6e3f954fd
Nest0rr <elemele58@gmail.com>
2019-12-08 11:54:24 +0100

    Initial Commit

---
+++ WhenItsColdOutside/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhenItsColdOutside
+//
+//  Created by Maciej Bałtowski on 08/12/2019.
+//  Copyright © 2019 Maciej. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
