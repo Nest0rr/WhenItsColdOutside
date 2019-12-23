664ac156c916fd94956d55f7695c465cdd9d1de8
Nest0rr <elemele58@gmail.com>
2019-12-23 22:13:57 +0100

    Add PickerView with a list of items

--- WhenItsColdOutside/Controller/ClothingPickerController.swift
+++ WhenItsColdOutside/Controller/ClothingPickerController.swift
@@ -7,11 +7,68 @@
 //
 
 import UIKit
+import CoreData
 
-class ClothingPickerController: UIViewController {
+//MARK: - TODO create a list of clothing and hook it up to pickerview.
+//implement CoreData so selected item can be saved
+
+class ClothingPickerController: UIViewController, UITextFieldDelegate {
+    let clothing = ["Tshirt",
+                        "Sweater",
+                        "Jacket",
+                        "Winter Jacket"]
         
-    @IBAction func saveClothing(_ sender: Any) {
+    var selectedClothing: String?
+    
+    @IBOutlet weak var clothingPickerTextField: UITextField!
+    
+    @IBAction func tappingTextField(_ sender: UITextField) {
+        createPickerView()
+        createToolBar()
     }
     
+    func createPickerView() {
+        let clothingPicker = UIPickerView()
+        clothingPicker.delegate = self
+        
+        clothingPickerTextField.delegate = self
+        clothingPickerTextField.inputView = clothingPicker
+        
+    }
     
+    func createToolBar() {
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(dismissKeyboard))
+        
+        toolbar.setItems([doneButton], animated: false)
+        toolbar.isUserInteractionEnabled = true
+        
+        clothingPickerTextField.inputAccessoryView = toolbar
+    }
+    
+    @objc func dismissKeyboard() {
+        clothingPickerTextField.endEditing(true)
+    }
+}
+
+extension ClothingPickerController: UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return clothing.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return clothing[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selectedClothing = clothing[row]
+        clothingPickerTextField.text = selectedClothing
+    }
 }
